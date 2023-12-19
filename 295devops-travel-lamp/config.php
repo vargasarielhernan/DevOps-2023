0b136e9f62fd8162ba74a4fd02e4c3308ec01dd5
vargasarielhernan <vargasarielhernan@gmail.com>
2023-12-18 21:37:14 -0300

    enlace de phpmyadmin con db msql funcionando

--- 295devops-travel-lamp/config.php
+++ 295devops-travel-lamp/config.php
@@ -1,8 +1,8 @@
 <?php
-$dbHost     = "db"; 
-$dbUsername = "codeuser"; 
-$dbPassword = "test"; 
-$dbName     = "devopstravel"; 
+$dbHost     = 'db'; 
+$dbUsername = 'codeuser'; 
+$dbPassword = 'test'; 
+$dbName     = 'devopstravel'; 
 $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
 if ($conn->connect_error) { 
     die("Connection failed: " . $conn->connect_error); 
